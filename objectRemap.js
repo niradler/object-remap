bc2a81795af04343285fd040dc2f5ff03fe05d32
Nir Adler <niradler@users.noreply.github.com>
2019-05-12 13:47:09 +0300

    Update objectRemap.js
    formatter

--- objectRemap.js
+++ objectRemap.js
@@ -2,7 +2,7 @@ const getField = require("lodash.get");
 const setField = require("lodash.set");
 
 function objectRemap(obj = {}, fields = []) {
-  return fields.reduce((newObj, fieldObj) => newObj = setField(newObj, fieldObj.target, fieldObj.formater && typeof fieldObj.formater === "function" ? fieldObj.formater(getField(obj, fieldObj.origin)) : getField(obj, fieldObj.origin)), {});
+  return fields.reduce((newObj, fieldObj) => newObj = setField(newObj, fieldObj.target, fieldObj.formatter && typeof fieldObj.formatter === "function" ? fieldObj.formatter(getField(obj, fieldObj.origin)) : getField(obj, fieldObj.origin)), {});
 }
 
 module.exports = objectRemap;
